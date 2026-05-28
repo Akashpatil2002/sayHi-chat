fb57aa380f7725070979079ba3b3d79b532806be
Akash Patil <patilakash2601@gmail.com>
2026-05-28 23:59:41 +0530

    Code Updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://sayhi-chat-5doa.onrender.com/api",
-  withCredentials: true, // ✅ MUST
+  withCredentials: true,
 });
